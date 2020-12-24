36830d0273c3edc59022671b7a455e59925712bb
Denis Palaguta <barmank@yandex.ru>
2020-12-24 18:14:14 +0300

    LB add file

---
+++ terraform/lb.tf
@@ -0,0 +1,39 @@
+/* provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+} */
+
+resource "yandex_lb_target_group" "app_group" {
+  name      = "app-target-group"
+#   region_id = "ru-central1"
+
+  target {
+    subnet_id = var.subnet_id
+    address   = yandex_compute_instance.app.network_interface.0.ip_address
+  }
+}
+
+resource "yandex_lb_network_load_balancer" "lb-app" {
+  name = "lb-app"
+
+  listener {
+    name = "my-listener"
+    port = 9292
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+  attached_target_group {
+    target_group_id = yandex_lb_target_group.app_group.id
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 9292
+        path = "/"
+      }
+    }
+  }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,6 @@
 output "external_ip_address_app" {
   value = yandex_compute_instance.app.network_interface.0.nat_ip_address
 }
+output "balancer_ip_address" {
+  value = [for ex_ip in yandex_lb_network_load_balancer.lb-app.listener: ex_ip.external_address_spec].0
+}
