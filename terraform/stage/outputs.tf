6902f647516ae7991f8dc503e197a3c71f6b5b4f
Denis Palaguta <barmank@yandex.ru>
2021-01-15 18:54:15 +0300

    Roles

--- terraform/stage/outputs.tf
+++ terraform/stage/outputs.tf
@@ -1,6 +1,12 @@
 output "external_ip_address_app" {
   value = module.app.external_ip_address_app
 }
+output "internal_ip_address_app" {
+  value = module.app.internal_ip_address_app
+}
 output "external_ip_address_db" {
   value = module.db.external_ip_address_db
+}
+output "internal_ip_address_db" {
+  value = module.db.internal_ip_address_db
 }
